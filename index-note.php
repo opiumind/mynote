44c6aa4c5b98f624d279a1f3f5bc1fa5f53545ee
opiumind <thebestladyfox@gmail.com>
2014-01-24 13:01:15 +0700

    Placeholders were done.

--- index-note.php
+++ index-note.php
@@ -85,33 +85,33 @@
 						print "]"; 
 					?>;
 			
-			function visibilityOfLabelsForAreas(id, focus){
-				var formName = "addition_form";
-				if (focus == "in"){
-					document.getElementById(id + "_label").className = "hidden_label";
-				}else{
-					if ($.trim(document.forms['addition_form']['code_input'].value) == ""){
-						document.getElementById(id + "_label").className = "";
-					}
-				}
-				
-			}
-			
-			function visibilityOfLabels(){
-				document.getElementById(a).onclick = function(){
-					$('')
-				}
-			}
-			function visibilityOfLabels(id, focus){
-				if (focus == "in"){
-					document.getElementById(id + "_label").className = "hidden_label";
-				}else{
-					if (document.getElementById(id).value == ""){
-						document.getElementById(id + "_label").className = "";
-					}
-				}
-				
-			}
+			// function visibilityOfLabelsForAreas(id, focus){
+				// var formName = "addition_form";
+				// if (focus == "in"){
+					// document.getElementById(id + "_label").className = "hidden_label";
+				// }else{
+					// if ($.trim(document.forms['addition_form']['code_input'].value) == ""){
+						// document.getElementById(id + "_label").className = "";
+					// }
+				// }
+// 				
+			// }
+// 			
+			// function visibilityOfLabels(){
+				// document.getElementById(a).onclick = function(){
+					// $('')
+				// }
+			// }
+			// function visibilityOfLabels(id, focus){
+				// if (focus == "in"){
+					// document.getElementById(id + "_label").className = "hidden_label";
+				// }else{
+					// if (document.getElementById(id).value == ""){
+						// document.getElementById(id + "_label").className = "";
+					// }
+				// }
+// 				
+			// }
 			
 			function showCategories(){
 				var listOfCategories = "";
@@ -254,8 +254,8 @@
 						return false;
 					});
 					//disapear of authentication and registration
-					$(document).click(function(event){
-				    	if( $(event.target).closest("#registration").length ) 
+					$(document).click(function(event) {
+				    	if( $(event.target).closest("#registration,#authentication").length ) 
 				        	return;
 				    	$("#registration").animate({
 				      		'top': '-8em'
@@ -265,17 +265,30 @@
 						}, "fast")
 						event.stopPropagation();
 				    });
+				    
 				    //show the form for adding
-				    $('p.add_button').click(function(){
+				    $('p.add_button').click(function() {
 				    	document.getElementById("add_note_popup").style.display = 'inline-block';
 				    	document.getElementById("cover").style.display = 'none';
 				    	return false;
 				    });
-					$('p.header').click(function(){
+					$('p.header').click(function() {
 				    	document.getElementById("cover").style.display = 'inline-block';
 				    	document.getElementById("add_note_popup").style.display = 'none';
 				    	return false;
 				    });
+				    //when focus on textarea
+				    $('input,textarea').focus(function() {
+				    	$(this).addClass('focused');
+				    	return false;
+				    });
+				    $('input,textarea').blur(function() {
+				    	if ($(this).val()=='') {
+				    		$(this).removeClass('focused');
+				    	}
+				    	return false;
+				    });
+				    
 				});
 			})(jQuery);
 			
@@ -290,7 +303,7 @@
 				</p>
 			</header>
 
-			<div id="menu" style="">
+			<div id="menu">
 				<div id="list_of_categories">
 				</div>
 				<div id="list_of_subcategories">
@@ -299,42 +312,34 @@
 			
 			
 			<div id="content">
-				<div id="cover" style="">
+				<div id="cover">
 					<img src="http://oboibox.ru/orig/Shkolnaya-tetrad%28oboibox.ru%29.jpg"/>
 				</div>
 				<div id="notes">
 					
 				</div>
-				<div id="add_note_popup" style="display: none;">
-					<form method="post" class="input_text" name="addition_form">
+				<!-- <div id="add_note_popup"> -->
+					<form id="add_note_popup" method="post" class="input_text" name="addition_form">
+						<input id="category_input" type="text" list="list_of_categories_in_datalist" value="" placeholder="category" autocomplete="off"/>
+						<datalist id="list_of_categories_in_datalist">
+						</datalist>
+						<input id="subcategory_input" type="text" value="" placeholder="subcategory" autocomplete="off"
+								onfocus="" onblur="" />
 						<div>
-							<input id="category_input" type="text" list="list_of_categories_in_datalist" value="" />
-							<datalist id="list_of_categories_in_datalist">
-							</datalist>
-							<label id="category_input">category</label>
+							<textarea id="code_input" class="code_in_addition" name="code_input" placeholder="code"
+								onfocus="" onblur=""></textarea>
+							<!-- <label>code</label> -->
 						</div>
 						<div>
-							<input id="subcategory_input" type="text" value="" 
-								onfocus="visibilityOfLabels('subcategory_input','in');" onblur="visibilityOfLabels('subcategory_input','out');" />
-							<label id="subcategory_input_label" >subcategory</label>
+							<textarea id="text_input" class="text_in_addition" name="text_input" placeholder="description"
+								onfocus="" onblur=""></textarea>
+							<!-- <label>description</label> -->
 						</div>
-						<div>
-							<textarea id="code_input" class="code_in_addition" name="code_input" 
-								onfocus="visibilityOfLabelsForAreas('code_input','in');" onblur="visibilityOfLabelsForAreas('code_input','out');">
-							</textarea>
-							<label id="code_input_label">code</label>
-						</div>
-						<div>
-							<textarea id="text_input" class="text_in_addition" name="text_input"
-								onfocus="visibilityOfLabelsForAreas('text_input','in');" onblur="visibilityOfLabelsForAreas('text_input','out');">
-							</textarea>
-							<label id="text_input_label" >let explain now</label>
-						</div>
-						<div>
+						<div class="center_align">
 							<input type="button" class="button" value="Good for you"/>
 						</div>
 					</form>
-				</div>
+				<!-- </div> -->
 				<!-- content -->
 			</div>
 			
@@ -342,32 +347,22 @@
 			
 			<div id="authentication" class="" style="">
 				<form name="authentication" method="post" action="">
-					<input id="login_input" class="" type="text" name="e-mail" value="" 
-							onfocus="visibilityOfLabels('login_input','in');" onblur="visibilityOfLabels('login_input','out');">
-					<label id="login_input_label"></label>
-					
-					<input id="password_input" class="sign" type="text" name="password" value="" 
-							onfocus="visibilityOfLabels('password_input','in');" onblur="visibilityOfLabels('password_input','out');">
-					<label id="password_input_label"></label>
-					
-					<input type="submit" class="button sign" value="Sign in" onclick="">
+					<input id="login_input" class="" type="text" name="e-mail" value="" autocomplete="off" placeholder="e-mail"
+							onfocus="" onblur="">
+					<input id="password_input" class="sign" type="text" name="password" value="" autocomplete="off" placeholder="password"
+							onfocus="" onblur="">
+					<input type="submit" class="button" value="Sign in" onclick="">
 				</form>
 			</div>
 			
 			<div id="registration" class="" style="">
 				<form name="registration" method="post" action="">
-					<input id="login_input" class="" type="text" name="e-mail" value="" 
-							onfocus="visibilityOfLabels('login_input','in');" onblur="visibilityOfLabels('login_input','out');">
-					<!-- <label id="login_input_label"></label> -->
-					
-					<input id="password_input" class="" type="text" name="password" value="" 
-							onfocus="visibilityOfLabels('password_input','in');" onblur="visibilityOfLabels('password_input','out');">
-					<!-- <label id="password_input_label"></label> -->
-					
-					<input id="confirm_password_input" class="" type="text" name="confirm_password" value="" 
-							onfocus="visibilityOfLabels('password_input','in');" onblur="visibilityOfLabels('password_input','out');">
-					<!-- <label id="confirm_password_input_label"></label> -->
-					
+					<input id="login_input" class="" type="text" name="e-mail" value="" autocomplete="off" placeholder="e-mail"
+							onfocus="" onblur="">
+					<input id="password_input" class="" type="text" name="password" value="" autocomplete="off" placeholder="password"
+							onfocus="" onblur="">
+					<input id="confirm_password_input" class="" type="text" name="confirm_password" value="" autocomplete="off" placeholder="confirm password"
+							onfocus="" onblur="">
 					<input type="submit" class="button" value="Sign up" onclick="">
 				</form>
 			</div>
